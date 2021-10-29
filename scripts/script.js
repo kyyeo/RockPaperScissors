b2b9fc7ea88464e2c203424b414064f5ebb516e6
kyyeo <yeokaiyu@gmail.com>
2021-10-30 01:30:00 +0800

    updated RPS game

--- scripts/script.js
+++ scripts/script.js
@@ -12,6 +12,7 @@ function computerPlay() {
 }
 
 // a 3x3 matrix shows that there are 9 combinations 
+// function to play a round
 function playRound(playerSelection, computerSelection){
     comparePlayer = playerSelection.toLowerCase()
     compareComputer = computerSelection.toLowerCase()
@@ -45,51 +46,95 @@ function playRound(playerSelection, computerSelection){
 //define 'global' score variables, which are accessible to keepScore()
 let playerScore;
 let computerScore;
+let roundNumber;
 
-//play a game of 5 rounds
+// //play a game of 5 rounds
+// function game() {
+//     //reset the score whenever a new game is initialised
+//     playerScore = 0;
+//     computerScore = 0;
+    
+//     for (let i=1; i<=5; i++){
+//         //define the selections
+//         const computerSelected = computerPlay();
+//         const playerSelected = playerSelection();
+
+//         //play each round 
+//         let id = i.toString();
+//         let para = "Round " + i + ": " + playRound(playerSelected, computerSelected);
+//         console.log(para);
+//         createPara(id, para)
+//     }
+
+//     //print the game result after 5 rounds
+//     gameResult = checkWinner();
+//     createPara("gameResult",gameResult);
+//     console.log(gameResult);
+// }
+
+const playerBoard = document.getElementById("playerScore");
+const computerBoard = document.getElementById("computerScore");
+const resultBoard = document.getElementById("gameResult");
+
+//game() //better to run function when <body> loads
 function game() {
     //reset the score whenever a new game is initialised
     playerScore = 0;
     computerScore = 0;
+    roundNumber = 1;
 
-    for (let i=1; i<=5; i++){
-        //define the selections
-        const computerSelected = computerPlay();
-        const playerSelected = playerSelection();
-
-        //play each round 
-        let id = i.toString();
-        let para = "Round " + i + ": " + playRound(playerSelected, computerSelected);
-        console.log(para);
-        createPara(id, para)
-    }
-
-    //print the game result after 5 rounds
-    gameResult = checkWinner();
-    createPara("gameResult",gameResult);
-    console.log(gameResult);
+    playerBoard.textContent = playerScore;
+    computerBoard.textContent = computerScore;
 }
+const body = document.body;
+body.onload = game();
 
 
-//get player's choice with a prompt
-function playerSelection(){
-    let loop =true;
-    while(loop){
-        const playerSelection = window.prompt("Rock? Paper? Scissors?", "Rock");
-        if (playerSelection.toLowerCase() === 'rock' || playerSelection.toLowerCase() === 'paper' || playerSelection.toLowerCase() === 'scissors'){
-            loop=false;
-            return playerSelection;
-        }
+// update state of the game
+function updateGame(playerSelection) {
+    //game resets if there has been a winner
+    if(playerScore >= 5 || computerScore >= 5) {
+        game();
     }
-}
     
+    //define selections
+    const computerSelected = computerPlay();
+    const playerSelected = playerSelection;
+    
+    //play each round 
+    let para = "Round " + roundNumber + ": " + playRound(playerSelected, computerSelected);
+    console.log(para);
+    // createPara(roundNumber, para)
+    
+
+    roundNumber+=1;
+    resultBoard.textContent = checkWinner();
+}
+
+
+// //get player's choice with a prompt
+// function playerSelection(){
+//     let loop =true;
+//     while(loop){
+//         const playerSelection = window.prompt("Rock? Paper? Scissors?", "Rock");
+//         if (playerSelection.toLowerCase() === 'rock' || playerSelection.toLowerCase() === 'paper' || playerSelection.toLowerCase() === 'scissors'){
+//             loop=false;
+//             return playerSelection;
+//         }
+//     }
+// }
+
 
 //helper function to update score
 function keepScore(winner){
     if (winner.toLowerCase() === 'player'){
         playerScore++;
+        playerBoard.textContent = playerScore;
+        computerBoard.textContent = computerScore;
     } else if (winner.toLowerCase() === 'computer') {
         computerScore++;
+        playerBoard.textContent = playerScore;
+        computerBoard.textContent = computerScore;
     } else {
         console.error('keepScore() is undefined');
         return null;
@@ -98,24 +143,33 @@ function keepScore(winner){
 
 
 //helper function to check winner
-//first to 3 wins
 function checkWinner(){
-    if (playerScore >= 3) {
+    //first to 5 points wins
+    let winningScore = 5
+
+    if (playerScore >= winningScore) {
         return `Score is ${playerScore} (Player) : ${computerScore} (Computer) | Player wins!`;
-    } else if (computerScore >= 3){
+    } else if (computerScore >= winningScore){
         return `Score is ${playerScore} (Player) : ${computerScore} (Computer) | Computer wins!`;
     } else {
-        return `Score is ${playerScore} (Player) : ${computerScore} (Computer) | No winner after 5 rounds!`;
+        return `Score is ${playerScore} (Player) : ${computerScore} (Computer) | No winner yet!`;
     }
 }
 
-//print to <p> element in index.html
-function createPara(id, phrase) {
-    let para = document.createElement("P");
-    let text = document.createTextNode(phrase);
-    para.appendChild(text);
-    document.getElementById(id).appendChild(para);    
-}
+// //print to <p> element in index.html
+// function createPara(id, phrase) {
+//     let para = document.createElement("P");
+//     let text = document.createTextNode(phrase);
+//     para.appendChild(text);
+//     document.getElementById(id).appendChild(para);    
+// }
 
-//play a game when index.html loads
-//game() //better to run function when <body> loads
+
+// add click event listener to RPS buttons
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        // alert(button.id);
+        updateGame(button.id);
+    })
+})
